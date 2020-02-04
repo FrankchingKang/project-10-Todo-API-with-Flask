d06685f710f88632a9fc40925685fc8411f79c32
yi ching kang <frankyi7878@gmail.com>
2020-02-04 21:11:20 +0800

    fix get

--- resources/todos.py
+++ resources/todos.py
@@ -30,9 +30,9 @@ class TodoList(Resource):
         super().__init__()
 
     def get(self):
-        #todos = [marshal((todo), todo_fields)
-        #           for todo in models.Todo.select()]
-        return jsonify({'todo': [{'name':'frank'}]})
+        todos = [marshal((todo), todo_fields)
+                   for todo in models.Todo.select()]
+        return {'courses': courses}
 
     @marshal_with(todo_fields)
     def post(self):
